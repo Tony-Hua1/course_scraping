658245abdbbc45520533711ed570d8d130272a81
Tony Hua <tony7068@gmail.com>
2022-08-30 21:50:21 -0700

    Convert script into a single function

--- course_scraping.py
+++ course_scraping.py
@@ -12,27 +12,29 @@ service = Service('/home/tony/Downloads/chromedriver') # ver ChromeDriver 104.0.
 service.start()
 driver = webdriver.Remote(service.service_url)
 
-url = "https://www.deanza.edu/schedule/" # De Anza College
-driver.get(url)
+def list_courses(course_title):
+    url = "https://www.deanza.edu/schedule/" # De Anza College
+    driver.get(url)
 
-search_box = driver.find_element(By.ID,"tbSearch")
-search_box.send_keys("Data Abstraction and Structures") # Search for all Data Abstraction and Structures courses
-search_button = driver.find_element(By.CSS_SELECTOR,"button.btn.dark.full")
-search_button.click()
-time.sleep(5) # seconds
+    search_box = driver.find_element(By.ID,"tbSearch")
+##    search_box.send_keys("Data Abstraction and Structures") # Search for all Data Abstraction and Structures courses
+    search_box.send_keys(course_title)
+    search_button = driver.find_element(By.CSS_SELECTOR,"button.btn.dark.full")
+    search_button.click()
+    time.sleep(5) # seconds
 
-print(driver.title)
+    page = driver.execute_script("return document.body.innerHTML;")
+    doc = BeautifulSoup(page, "html.parser")
+    tbody = doc.tbody
+    trs = tbody.contents
 
-page = driver.execute_script("return document.body.innerHTML;")
-doc = BeautifulSoup(page, "html.parser")
-tbody = doc.tbody
-trs = tbody.contents
+    # Print info for each course section
+    for tr in trs[1::2]:
+        for td in tr.find_all("td"):
+            print(td.text)
 
-# Print info for each course section
-for tr in trs[1::2]:
-    for td in tr.find_all("td"):
-        print(td.text)
+    ##driver.refresh()
+    driver.close() # close tab
+    driver.quit() # close browser
 
-##driver.refresh()
-##driver.close() # close tab
-##driver.quit() # close browser
+list_courses("Data Abstraction and Structures")
